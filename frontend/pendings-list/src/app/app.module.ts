fbc06b69ce36ee1abc2e008e762155ad54964342
Esther Manrique <esther_manrique@epam.com>
2022-07-25 18:44:55 -0500

    Added sigle-task component and icons

--- frontend/pendings-list/src/app/app.module.ts
+++ frontend/pendings-list/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { SigleTaskComponent } from './sigle-task/sigle-task.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
     HeaderComponent,
     FooterComponent,
     TasksComponent,
+    SigleTaskComponent,
   ],
   imports: [
     BrowserModule,
